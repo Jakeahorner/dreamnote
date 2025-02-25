0fc6c1b2cae744053f746cb599ed55d38a5429d6
Jake Horner <jakeahorner@gmail.com>
2025-02-25 10:05:43 -0600

    made placeholder toolbar and made background a table

--- lib/src/Editor/Background.dart
+++ lib/src/Editor/Background.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'Toolbar.dart';
+
 class Background extends StatefulWidget {
   final Widget child;
   final bool isMoving;
@@ -30,22 +32,36 @@ class _BackgroundState extends State<Background> {
     }
 
     return(
-    ColoredBox(
+    ColoredBox (
       color: Colors.grey,
-        child: InteractiveViewer(
-          onInteractionStart: onInteractionStart,
-          onInteractionEnd: onInteractionEnd,
-          panEnabled: widget.isMoving && (pointerCount == 2),
-          scaleEnabled: widget.isMoving && (pointerCount == 2),
-          minScale: 0.1,
+      child: Column(
+        children: [
+          Row(
+            children: [Toolbar()],
+          ),
+          Row(
+            children: [
+              InteractiveViewer(
+                    onInteractionStart: onInteractionStart,
+                    onInteractionEnd: onInteractionEnd,
+                    panEnabled: widget.isMoving && (pointerCount == 2),
+                    scaleEnabled: widget.isMoving && (pointerCount == 2),
+                    minScale: 0.1,
 
-          child: SizedBox(
-        height: 500,
-        width: 200,
-            child: widget.child,
-        ),
-      )
+                    child: SizedBox(
+                      height: 500,
+                      width: 300,
+                      child: widget.child,
+                    ),
+                  )
+            ],
+          )
+        ],
+      ),
     )
+
+
+
 
     );
   }
--- lib/src/Editor/Toolbar.dart
+++ lib/src/Editor/Toolbar.dart
@@ -2,51 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Toolbar extends StatefulWidget {
-  final Widget child;
-  final bool isMoving;
-  const Toolbar({super.key, required this.child, required this.isMoving});
+  const Toolbar({super.key});
 
   @override
   State<Toolbar> createState() => _ToolbarState();
 }
 
-
 class _ToolbarState extends State<Toolbar> {
-  int pointerCount = 0;
 
   @override
   Widget build(BuildContext context) {
-
-    void onInteractionStart(ScaleStartDetails event) {
-      setState(() {
-        pointerCount = event.pointerCount;
-      });
-    }
-
-    void onInteractionEnd(ScaleEndDetails event) {
-      setState(() {
-        pointerCount = event.pointerCount;
-      });
-    }
-
     return(
-        ColoredBox(
-            color: Colors.grey,
-            child: InteractiveViewer(
-              onInteractionStart: onInteractionStart,
-              onInteractionEnd: onInteractionEnd,
-              panEnabled: widget.isMoving && (pointerCount == 2),
-              scaleEnabled: widget.isMoving && (pointerCount == 2),
-              minScale: 0.1,
-
-              child: SizedBox(
-                height: 500,
-                width: 200,
-                child: widget.child,
-              ),
-            )
-        )
-
+    Text("hello")
     );
   }
 }
