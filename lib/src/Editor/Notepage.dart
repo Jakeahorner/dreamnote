cfccc00abf7631fd327e04bc6403ed9e00d0ed72
jakeahorner <Jakeahorner@gmail.com>
2025-05-16 01:23:16 -0500

    working eraser, definiltly look more into it

--- lib/src/Editor/Notepage.dart
+++ lib/src/Editor/Notepage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/scheduler.dart';
+import 'package:r_tree/r_tree.dart';
+import 'dart:math';
 import '../Utilities/PointerDetails.dart';
 import '../Utilities/Tool.dart';
 
@@ -14,10 +16,11 @@ class Notepage extends StatefulWidget {
   State<Notepage> createState() => _Notepage();
 }
 
-class _Notepage extends State<Notepage> {
+class _Notepage extends State<Notepage> with WidgetsBindingObserver{
 
   Path path = Path();
   List<Path> pathStack = [];
+  RTree<Path> pathTree = RTree<Path>();
 
   void onPointerDown(PointerDownEvent event) {
     setState(() {
@@ -51,11 +54,13 @@ class _Notepage extends State<Notepage> {
       if (widget.isTooling && !widget.pointerDetails.isMuliTouched() &&
           widget.pointerDetails.getTool() == Tool.pen) {
         pathStack.add(path);
+        pathTree.add([RTreeDatum(Rectangle.fromPoints(Point(path.getBounds().topLeft.dx, path.getBounds().topLeft.dy), Point(path.getBounds().bottomRight.dx, path.getBounds().bottomRight.dy)), path)]);
       }
       path = Path();
       widget.pointerDetails.removePointer(event);
     });
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +74,8 @@ class _Notepage extends State<Notepage> {
               foregroundPainter: Painter(
                   pointerDetails: widget.pointerDetails,
                   pathStack: pathStack,
-                  path: path),
+                  path: path,
+                  pathTree: pathTree),
               child:
                 SizedBox.expand(
                   child: ColoredBox(color: Colors.white),
@@ -84,11 +90,13 @@ class Painter extends CustomPainter {
   final Path path;
   List<Path> pathStack = [];
   PointerDetails pointerDetails;
+  RTree<Path> pathTree;
 
   Painter({
     required this.pointerDetails,
     required this.path,
     required this.pathStack,
+    required this.pathTree,
   });
 
   @override
@@ -128,22 +136,51 @@ class Painter extends CustomPainter {
         canvas.drawPath(path, penPaint);
 
       case Tool.eraser:
-        for (Path path in pathStack) {
-          Path intersection =
-          Path.combine(PathOperation.union, this.path, path);
-          print(intersection.getBounds());
-          if (intersection.getBounds().isEmpty) {
-            pathStack.remove(path);
+        Offset eraserPoint = pointerDetails.stylus.hoveringLocation;
+        List<RTreeDatum<Path>> eraserPaths = pathTree.search(Rectangle.fromPoints(Point(path.getBounds().topLeft.dx, path.getBounds().topLeft.dy), Point(path.getBounds().bottomRight.dx, path.getBounds().bottomRight.dy)));
+
+        for (RTreeDatum<Path> testerPath in eraserPaths) {
+          print(pathsIntersect(testerPath.value, path));
+
+          if(pathsIntersect(testerPath.value, path)) {
+            pathStack.remove(testerPath.value);
           }
         }
     }
   }
+  bool pathsIntersect(Path pathA, Path pathB, {double tolerance = 2.0}) {
+    if (!pathA.getBounds().inflate(tolerance).overlaps(pathB.getBounds())) return false;
 
-// Since this Sky painter has no fields, it always paints
-// the same thing and semantics information is the same.
-// Therefore we return false here. If we had fields (set
-// from the constructor) then we would return true if any
-// of them differed from the same fields on the oldDelegate.
+    final metricsA = pathA.computeMetrics(forceClosed: false).toList();
+    final metricsB = pathB.computeMetrics(forceClosed: false).toList();
+
+    // Precompute sample points from B for quick lookup
+    final List<Offset> pathBSamples = [];
+    for (final metricB in metricsB) {
+      for (double t = 0; t < metricB.length; t += 4.0) {
+        final posB = metricB.getTangentForOffset(t)?.position;
+        if (posB != null) pathBSamples.add(posB);
+      }
+    }
+
+    // Check if any point from A is close to any point from B
+    for (final metricA in metricsA) {
+      for (double t = 0; t < metricA.length; t += 4.0) {
+        final posA = metricA.getTangentForOffset(t)?.position;
+        if (posA == null) continue;
+
+        for (final posB in pathBSamples) {
+          if ((posA - posB).distance <= tolerance) {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+
   @override
   bool shouldRepaint(Painter oldDelegate) => true;
 }
